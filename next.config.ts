a5e5608d4c05e32c6054a88bc30d6c3bef833f8d
Mateja <markojovanovicc23@outlook.com>
2026-04-02 15:39:38 +0200

    added optimizations in next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  cacheComponents: true
+  cacheComponents: true,
+  images: {
+    formats: ['image/avif', 'image/webp']
+  },
+  compress: true,
+  productionBrowserSourceMaps: false,
+  output: "standalone",
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  experimental: {
+    optimizePackageImports: ['lucide-react', 'framer-motion'],
+  },
 };
 
 export default nextConfig;
